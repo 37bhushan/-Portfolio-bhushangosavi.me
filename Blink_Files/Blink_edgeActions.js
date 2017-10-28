04208b4f02fda7b2edc427fe7ce9c81f8fe982db
37bhushan <37bhushan@gmail.com>
2017-10-28 12:14:21 +0530

    Initial Commit
    Moved from github.io to repo

---
+++ Blink_Files/Blink_edgeActions.js
@@ -0,0 +1,10 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",13250,function(sym,e){this.play(0);});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",13254,function(sym,e){this.play();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-325458644");
---
+++ Blink_Mob/blink_mob_edgeActions.js
@@ -0,0 +1,8 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",6000,function(sym,e){this.play();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-35921690");
